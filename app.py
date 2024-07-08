1862f1ca50e81660418a62dcc4843f6f82cdd415
mariaherliana <150203908+mariaherliana@users.noreply.github.com>
2024-07-08 16:12:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,19 @@ from src.csv_processing import process_console_csv, process_dashboard_csv, save_
 from src.CallDetail import CallDetail
 from src.idn_area_codes import EMERGENCY_NUMBERS, PHONE_PREFIXES, INTERNATIONAL_PHONE_PREFIXES
 from src.utils import parse_jakarta_datetime, convert_to_jakarta_time_iso, call_hash, classify_number, format_datetime_as_human_readable, format_timedelta, format_username, parse_call_memo, parse_iso_datetime
+from src.FileConfig import Config, Files
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    # Extract the client name from the first Files object in the CONFIG list
+    client_name = CONFIG[0].client
+
     # Call your existing script to process the CSV files
     call_details = {}
-    call_details = process_dashboard_csv("path/to/dashboard.csv", call_details)
-    call_details = process_console_csv("path/to/console.csv", call_details)
+    call_details = process_dashboard_csv(CONFIG[0].dashboard, call_details)
+    call_details = process_console_csv(CONFIG[0].console, call_details)
 
     # Convert the call_details dictionary to a DataFrame
     df = pd.DataFrame([call_detail.to_dict() for call_detail in call_details.values()])
@@ -22,7 +26,7 @@ def index():
     total_call_charge = sum(float(call_detail.call_charge) for call_detail in call_details.values())
 
     # Render the HTML template with the DataFrame and total call charge
-    return render_template('index.html', data=df.to_dict(orient="records"), total_call_charge=total_call_charge)
+    return render_template('index.html', data=df.to_dict(orient="records"), total_call_charge=total_call_charge, client_name=client_name)
 
 if __name__ == '__main__':
     app.run()
